fdf1de29071d5c32b58ceaaef46d1ebbd1315ef6
champark <cpkrcd1992@gmail.com>
2023-04-15 23:33:53 +0900

    사소한 오타로 시간을 조금 쓴 것 말고는 특이점 없음 $ 와 & 의 차이를 유의할것....

--- Spring_Board1/src/main/java/kr/co/service/BoardService.java
+++ Spring_Board1/src/main/java/kr/co/service/BoardService.java
@@ -3,7 +3,7 @@ package kr.co.service;
 import java.util.List;
 
 import kr.co.vo.BoardVO;
-import kr.co.vo.Criteria;
+import kr.co.vo.SearchCriteria;
 
 public interface BoardService {
 	
@@ -14,10 +14,10 @@ public interface BoardService {
 	public BoardVO read(int bno) throws Exception;
 	
 	// 게시물 목록 조회
-	public List<BoardVO> list(Criteria cri) throws Exception;
+	public List<BoardVO> list(SearchCriteria scri) throws Exception;
 	
 	// 게시물 총 갯수
-	public int listCount() throws Exception;
+	public int listCount(SearchCriteria scri) throws Exception;
 	
 	// 게시물 수정
 	public void update(BoardVO boardVO) throws Exception;
